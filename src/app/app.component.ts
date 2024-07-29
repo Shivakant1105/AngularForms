740b0e912bad47de22772732ae65f91894b9b030
Shiva Kant <kanthshiva1105@gmail.com>
2024-07-29 15:29:14 +0530

    using Form Data display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,34 @@ export class AppComponent
   @ViewChild ('form') myFrom :NgForm
   defaultQuestion="pet"
   answer=''
+
+  user={
+    username:'',
+    email:'',
+    secret:'',
+    question:''
+  }
+  submitted=false
   suggestUserName() {
     const suggestedName = 'Superuser';
+// set the value in my form using this mesthod
+    // this.myFrom.setValue({
+    //   userData:{  
+    //     username:suggestedName,
+    //     email:''
+    //   },
+    //   secret:'',
+    //   question:''
+    // })
+
+    // this below apporch is best way patch value only overwrite specific method 
+
+
+    this.myFrom.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    })
   }
 
 
@@ -24,9 +50,18 @@ export class AppComponent
 
 // }
 
-// here use refernce of my form from using viewchild
+// here use reference of my form from using viewchild
 onSubmit(){
-console.log(this.myFrom);
 
+console.log(this.myFrom);
+//  Display data  Using Form Data
+this.submitted=true
+this.user.username=this.myFrom.value.userData.username
+this.user.email=this.myFrom.value.userData.email
+this.user.secret=this.myFrom.value.secret
+this.user.question=this.myFrom.value.question  
+
+// reset my form
+this.myFrom.reset()
 }
 }
